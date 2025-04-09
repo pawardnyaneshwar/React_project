968a6c095293d750610cef8d8d99800c00fb03dc
Dnyaneshwarpawar <dpawar8530@gmail.com>
2025-04-10 00:13:29 +0530

    viteproject

---
+++ my-app/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Chai from './chai';
+
+
+function App() {
+  return (
+    <div className="App">
+        <p>1 st react  project </p>
+        <Chai/>
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/chai.js
@@ -0,0 +1,7 @@
+function Chai() {
+    return (
+        <h1>new  chai with  react </h1>
+    )
+}
+
+export default Chai;
